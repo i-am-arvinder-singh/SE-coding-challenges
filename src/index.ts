15364ddbf3c4801999145873b631a540de80c1e1
i-am-arvinder-singh <avyarvinder@gmail.com>
2024-01-27 20:57:33 +0530

    setting up projet

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { main } from './wc-tool/main';
+
+main();
+
---
+++ src/wc-tool/main.ts
@@ -0,0 +1,4 @@
+// Define your main function or entry point here
+export const main = () => {
+    console.log("Hello, world!");
+}
